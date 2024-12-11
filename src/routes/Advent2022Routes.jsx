09e056ae0020c3c633aa705d39a28224b52b9cf3
ubungmeister <ubungmeister@gmail.com>
2024-12-11 14:59:43 +0100

    task 2024-2

--- src/routes/Advent2022Routes.jsx
+++ src/routes/Advent2022Routes.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Task1 from '../pages/advent2024/Task1';
+import Task1 from '../pages/advent2022/Task1';
+import Task2 from '../pages/advent2022/Task2';
+import Task3 from '../pages/advent2022/Task3';
+import Task6 from '../pages/advent2022/Task6';
+import Task7 from '../pages/advent2022/Task7';
+import Task9 from '../pages/advent2022/Task9';
+import Task14 from '../pages/advent2022/Task14';
 
 const Advent2022Routes = [
-  <Route key="task1" path="/2024_1" element={<Task1 />} />,
+  <Route key="task1" path="/task1" element={<Task1 />} />,
+  <Route key="task2" path="/task2" element={<Task2 />} />,
+  <Route key="task3" path="/task3" element={<Task3 />} />,
+  <Route key="task6" path="/task6" element={<Task6 />} />,
+  <Route key="task7" path="/task7" element={<Task7 />} />,
+  <Route key="task9" path="/task9" element={<Task9 />} />,
+  <Route key="task14" path="/task14" element={<Task14 />} />
 ];
 
 export default Advent2022Routes;
